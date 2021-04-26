0fb25e8f24fe45c08c0a316790daca3247a436cc
sakitkepala <andikapriyotama@gmail.com>
2021-04-26 20:38:14 +0700

    Fetch data list akunnya dari back end

--- src/screens/budget.js
+++ src/screens/budget.js
@@ -1,12 +1,28 @@
 import React from "react";
 import { InitialBudget } from "./budget-welcome";
 import { ManajemenBudget } from "./budget-manajemen";
+import { client as apiClient } from "../utils";
 
 function BudgetScreen() {
   // TODO: implemen pakai context?
   const [listAkun, setListAkun] = React.useState(null);
 
-  return !listAkun ? (
+  React.useEffect(() => {
+    if (listAkun) {
+      return;
+    }
+
+    apiClient("/akun").then(
+      (respon) => {
+        setListAkun(respon.data);
+      },
+      (error) => {
+        console.log("Error:", error.message);
+      }
+    );
+  }, [listAkun]);
+
+  return !listAkun || listAkun.length === 0 ? (
     <InitialBudget onSimpanAkun={setListAkun} />
   ) : (
     <ManajemenBudget />
