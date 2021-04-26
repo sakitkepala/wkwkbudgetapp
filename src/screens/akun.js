60b2860ebd740b1e2525a6a6f5eb3a1c890e7f14
sakitkepala <andikapriyotama@gmail.com>
2021-04-26 19:51:00 +0700

    Fetch data akun dari screen akun dana

--- src/screens/akun.js
+++ src/screens/akun.js
@@ -1,21 +1,34 @@
+import React from "react";
+import { client as apiClient } from "../utils";
 import { Button } from "@chakra-ui/button";
 import { Box, Center, Flex, Text } from "@chakra-ui/layout";
 import { Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/table";
 
-const akunDummy = [
-  { id: 1, nama: "BCA", jenis: "Bank", dana: 4000000 },
-  { id: 2, nama: "Tunai", jenis: "Tunai", dana: 500000 },
-];
-
-// const akunDummy = [];
-
 function AkunDanaScreen() {
+  const [dataAkun, setDataAkun] = React.useState(null);
+
+  React.useEffect(() => {
+    if (dataAkun) {
+      return;
+    }
+
+    apiClient("/akun").then(
+      (respon) => {
+        setDataAkun(respon.data);
+      },
+      (error) => {
+        console.log("Tidak ada data. Pesan error:", error.message);
+      }
+    );
+  }, [dataAkun]);
+
   return (
     <Flex direction="column" alignItems="center">
       <Box
         as="main"
         width="60%"
         mt="24"
+        pb="12"
         borderRadius="md"
         shadow="base"
         bgColor="white"
@@ -31,7 +44,7 @@ function AkunDanaScreen() {
             </Tr>
           </Thead>
           <Tbody>
-            {akunDummy?.map(({ id, nama, jenis, dana }) => (
+            {dataAkun?.map(({ id, nama, jenis, dana }) => (
               <Tr key={id}>
                 <Td>{nama}</Td>
                 <Td>{jenis}</Td>
@@ -41,7 +54,7 @@ function AkunDanaScreen() {
           </Tbody>
         </Table>
 
-        {akunDummy.length === 0 ? (
+        {!dataAkun || dataAkun.length === 0 ? (
           <Center flexDir="column" h="250px">
             <Text color="gray.500">Belum punya akun. Buat dulu ya.</Text>
             <Button m="8">+ Buat Akun</Button>
