d6d70ce89278c909814b46f0c6d973983eda40dd
sakitkepala <andikapriyotama@gmail.com>
2021-04-28 22:44:49 +0700

    Compute dana dianggarkan dari line dana

--- src/test/data/budget.js
+++ src/test/data/budget.js
@@ -1,13 +1,30 @@
 import dataBudget from "./data-budget.json";
+import dataDanaLine from "./data-danaLine.json";
 
 let listBudget = [...dataBudget];
 let latestBudget = dataBudget[dataBudget.length - 1];
 
+const computeDanaDianggarkan = (budgetId) => {
+  const totalAwal = 0;
+  const reducerTotal = (total, line) => {
+    if (line.budgetId === budgetId) {
+      return total + line.jumlah;
+    }
+    return 0;
+  };
+  const totalDana = dataDanaLine.reduce(reducerTotal, totalAwal);
+  return totalDana;
+};
+
 async function read(budgetId) {
-  if (budgetId) {
-    return listBudget.find((data) => data.id === budgetId);
-  }
-  return latestBudget;
+  const budget = !budgetId
+    ? latestBudget
+    : listBudget.find((data) => data.id === budgetId);
+
+  return {
+    ...budget,
+    danaDianggarkan: computeDanaDianggarkan(budget.id),
+  };
 }
 
 export { read };
---
+++ src/test/data/danaLine.js
@@ -0,0 +1,7 @@
+import dataDanaLine from "./data-danaLine.json";
+
+async function readAll() {
+  return dataDanaLine;
+}
+
+export { readAll };
