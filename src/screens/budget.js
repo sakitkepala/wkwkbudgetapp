3a92edf005474b79ab1aafc9485f863f412d1fe8
sakitkepala <andikapriyotama@gmail.com>
2021-04-28 20:12:29 +0700

    REF logika fetch ke hook custom useBudgetSetup

--- src/screens/budget.js
+++ src/screens/budget.js
@@ -1,32 +1,52 @@
 import React from "react";
-import { InitialBudget } from "./budget-welcome";
-import { ManajemenBudget } from "./budget-manajemen";
+import { InitialBudget as InitBudgetScreen } from "./budget-welcome";
+import { ManajemenBudget as ManajemenBudgetScreen } from "./budget-manajemen";
 import { client as apiClient } from "../utils";
 
-function BudgetScreen() {
-  // TODO: implemen pakai context?
+function useBudgetSetup() {
+  const [isLoading, setIsLoading] = React.useState(false);
+  // Setup sementara cuma cek data list akun
   const [listAkun, setListAkun] = React.useState(null);
 
+  const isBudgetSiap = Boolean(listAkun) || listAkun?.length > 0;
+  const budgetSetupData = {
+    akun: listAkun,
+  };
+
   React.useEffect(() => {
-    if (listAkun) {
+    if (isBudgetSiap || isLoading) {
       return;
     }
 
+    setIsLoading(true);
     apiClient("/akun").then(
       (respon) => {
+        setIsLoading(false);
         setListAkun(respon.data);
       },
       (error) => {
+        setIsLoading(false);
         console.log("Error:", error.message);
       }
     );
-  }, [listAkun]);
+  }, [isBudgetSiap, isLoading]);
 
-  return !listAkun || listAkun.length === 0 ? (
-    <InitialBudget onSimpanAkun={setListAkun} />
-  ) : (
-    <ManajemenBudget />
-  );
+  return {
+    isLoading,
+    isBudgetSiap,
+    budgetSetupData,
+    dispatch: (value) => setListAkun(value),
+  };
+}
+
+function BudgetScreen() {
+  // TODO: implemen dispatch & value pakai context => <BudgetSetupProvider /> ?
+  const { isBudgetSiap, dispatch } = useBudgetSetup();
+
+  if (!isBudgetSiap) {
+    return <InitBudgetScreen onSimpanAkun={(data) => dispatch(data)} />;
+  }
+  return <ManajemenBudgetScreen />;
 }
 
 export { BudgetScreen };
