b9a81e315741993f72e551e4d1208e81f6f08ace
sakitkepala <andikapriyotama@gmail.com>
2021-04-29 02:58:58 +0700

    Housekeeping

--- src/components/budget-line.js
+++ src/components/budget-line.js
@@ -35,39 +35,41 @@ function BudgetLinesDataView({ bulan }) {
         color="gray.500"
       >
         <Text>To be developed...</Text>
-
-        {/* <Heading as="h2" size="md">
-          {!budgetLines ? "" : budgetLines[0].kategori}
-        </Heading>
-        <Text mt="1em">{!budgetLines ? "" : budgetLines[0].kategori}</Text>
-
-        <Table variant="simple" size="sm" colorScheme="gray" mt="1em">
-          <Tbody>
-            <Tr>
-              <Td>Dianggarkan</Td>
-              <Td isNumeric align="right">
-                Rp {!budgetLines ? 0 : budgetLines[0].dianggarkan},00
-              </Td>
-            </Tr>
-            <Tr>
-              <Td>Dibelanjakan</Td>
-              <Td isNumeric align="right">
-                Rp {!budgetLines ? 0 : budgetLines[0].terpakai},00
-              </Td>
-            </Tr>
-            <Tr>
-              <Td>Tersisa</Td>
-              <Td isNumeric align="right">
-                Rp 0,00
-              </Td>
-            </Tr>
-          </Tbody>
-        </Table> */}
+        {/* {kontenInfoDetail} */}
       </Box>
 
       {!budgetLines ? "Loading..." : <TabelBudget data={budgetLines} />}
     </Box>
   );
 }
+
+// const kontenInfoDetail = [
+//   <Heading as="h2" size="md">
+//     {!budgetLines ? "" : budgetLines[0].kategori}
+//   </Heading>,
+//   <Text mt="1em">{!budgetLines ? "" : budgetLines[0].kategori}</Text>,
+//   <Table variant="simple" size="sm" colorScheme="gray" mt="1em">
+//     <Tbody>
+//       <Tr>
+//         <Td>Dianggarkan</Td>
+//         <Td isNumeric align="right">
+//           Rp {!budgetLines ? 0 : budgetLines[0].dianggarkan},00
+//         </Td>
+//       </Tr>
+//       <Tr>
+//         <Td>Dibelanjakan</Td>
+//         <Td isNumeric align="right">
+//           Rp {!budgetLines ? 0 : budgetLines[0].terpakai},00
+//         </Td>
+//       </Tr>
+//       <Tr>
+//         <Td>Tersisa</Td>
+//         <Td isNumeric align="right">
+//           Rp 0,00
+//         </Td>
+//       </Tr>
+//     </Tbody>
+//   </Table>,
+// ];
 
 export { BudgetLinesDataView };
