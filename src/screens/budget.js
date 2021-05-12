797effff01c283b7d7a2c6d170f781cbc1fed209
sakitkepala <andikapriyotama@gmail.com>
2021-05-12 22:04:25 +0700

    Sederhanakan screen budget dulu, biar gak salah abstraksi

--- src/screens/budget.js
+++ src/screens/budget.js
@@ -1,54 +1,119 @@
 import React from "react";
-import { InitialBudget as InitBudgetScreen } from "./budget-welcome";
-import { ManajemenBudgetScreen } from "./budget-manajemen";
-import { client as apiClient } from "../utils";
+import { Box, Center, chakra } from "@chakra-ui/react";
+import { BudgetLinesDataView } from "../components/budget-line";
+import { useDanaBulanIni } from "../utils/dana-lines";
+import { useBudgetLines } from "../utils/budget-lines";
+import { useDanaBudget } from "../utils/budget-dana";
 
-function useBudgetSetup() {
-  const [isLoading, setIsLoading] = React.useState(false);
-  // Setup sementara cuma cek data list akun
-  const [listAkun, setListAkun] = React.useState(null);
-
-  const isBudgetSiap = Boolean(listAkun) || listAkun?.length > 0;
-  const budgetSetupData = {
-    akun: listAkun,
-  };
-
-  React.useEffect(() => {
-    if (isBudgetSiap || isLoading) {
-      return;
-    }
-
-    setIsLoading(true);
-    apiClient("/akun").then(
-      (respon) => {
-        setIsLoading(false);
-        setListAkun(respon.data);
-      },
-      (error) => {
-        setIsLoading(false);
-        console.error("Error:", error.message);
-      }
-    );
-  }, [isBudgetSiap, isLoading]);
-
-  return {
-    isLoading,
-    isBudgetSiap,
-    budgetSetupData,
-    dispatch: (value) => setListAkun(value),
-  };
+function DisplayBulan(props) {
+  return (
+    <Box
+      className="display-bulan"
+      textTransform="uppercase"
+      fontSize="2xl"
+      color="gray.300"
+      {...props}
+    >
+      {props.children}
+    </Box>
+  );
 }
 
-function BudgetScreen() {
-  // TODO: implemen dispatch & value pakai context => <BudgetSetupProvider /> ?
-  const { isBudgetSiap, dispatch } = useBudgetSetup();
+const totalByField = (arr, fieldJumlah) => {
+  return arr.length > 0
+    ? arr.reduce((total, line) => total + line[fieldJumlah], 0)
+    : 0;
+};
 
-  // TODO: budget setup & pengecekan budget siap ini baiknya di provider authenticated app
-  // biar gak ada flash form waktu lagi loading data
-  if (!isBudgetSiap) {
-    return <InitBudgetScreen onSimpanAkun={(data) => dispatch(data)} />;
+function DisplayDanaBudget({ danaLines, budgetLines }) {
+  const dana = useDanaBudget();
+
+  React.useEffect(() => {
+    if (!danaLines.data) {
+      return;
+    }
+    const totalDana = totalByField(danaLines.data, "jumlah");
+    dana.setTotal(totalDana);
+  }, [dana, danaLines.data]);
+
+  React.useEffect(() => {
+    if (!budgetLines.data) {
+      return;
+    }
+    const totalBudget = totalByField(budgetLines.data, "dianggarkan");
+    dana.setTerpakai(totalBudget);
+  }, [dana, budgetLines.data]);
+
+  return (
+    <Box
+      py="12"
+      px="8"
+      mt="12"
+      bgColor="gray.50"
+      borderWidth="1px"
+      borderColor="gray.200"
+      borderRadius="lg"
+      shadow="base"
+      fontSize="4xl"
+      fontWeight="bold"
+      color="gray.500"
+    >
+      <chakra.span fontWeight="normal" color="gray.300">
+        Rp
+      </chakra.span>{" "}
+      {dana.sisa}
+      <chakra.span fontWeight="normal" color="gray.300">
+        ,00
+      </chakra.span>
+    </Box>
+  );
+}
+
+function namaBulan(bulan) {
+  switch (bulan) {
+    case 11:
+      return "Desember";
+    default:
+      console.error("Bulan gak disupport");
   }
-  return <ManajemenBudgetScreen />;
+}
+
+function getBulan(dummyBulan) {
+  return dummyBulan || new Date().getMonth();
+}
+
+/**
+ * Screen manajemen budget menampilkan informasi untuk budgeting tiap bulannya,
+ * sehingga data yang tampil adalah data budgeting untuk bulan ini.
+ *
+ * Data dan state yang dibutuhkan untuk menampilkan UI di sini:
+ * 1. sekarang bulan apa?
+ * 2. berapa dana yang tersedia untuk budgeting?
+ * 3. apa saja yang dibudget untuk bulan ini?
+ * ...
+ */
+
+function BudgetScreen() {
+  // Tiap render bisa secara otomatis dapat data Date bulan dan tahun sekarang
+  // dengan menginstansiasi objek Date. Kemudian, karena requirement saat ini
+  // belum perlu mengelola bulan & tahun saat ini sebagai state karena tidak ada
+  // kondisi untuk mengubah state tersebut di renderan saat ini, jadi hook di
+  // bawah bisa dengan aman mengasumsikan date saat ini langsung dari inisiasi
+  // objek `Date()` saja.
+  const danaLines = useDanaBulanIni();
+  const budgetLines = useBudgetLines();
+
+  const bulan = getBulan(11);
+
+  return (
+    <Box>
+      <Center flexDirection="column">
+        <DisplayBulan mt="12">{namaBulan(bulan)}</DisplayBulan>
+        <DisplayDanaBudget danaLines={danaLines} budgetLines={budgetLines} />
+      </Center>
+      <BudgetLinesDataView budgetLines={budgetLines} />
+    </Box>
+  );
 }
 
 export { BudgetScreen };
