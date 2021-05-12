a2f2fc49003d1f849ae4075af364549baf5c2736
sakitkepala <andikapriyotama@gmail.com>
2021-05-12 14:43:10 +0700

    Housekeeping, hapus komen gak kepakai.

--- src/test/server/index.js
+++ src/test/server/index.js
@@ -1,7 +1,5 @@
-// if (process.env.NODE_ENV === "development" && typeof window !== "undefined") {
-//   module.exports = require("./dev-server");
-// }
-
+// Supaya server msw untuk demo di gh pages bisa jalan, dia perlu juga diekspor
+// bahkan ketika production, gak cuman development.
 if (typeof window !== "undefined") {
   module.exports = require("./dev-server");
 }
