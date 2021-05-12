5b9ac4cb6a30fa7bf46f20c0fd573ce758541f99
sakitkepala <andikapriyotama@gmail.com>
2021-05-12 20:02:22 +0700

    Setup kondisi server production

--- src/test/server/index.js
+++ src/test/server/index.js
@@ -1,5 +1,10 @@
 // Supaya server msw untuk demo di gh pages bisa jalan, dia perlu juga diekspor
 // bahkan ketika production, gak cuman development.
-if (typeof window !== "undefined") {
+if (process.env.NODE_ENV === "development" && typeof window !== "undefined") {
   module.exports = require("./dev-server");
+} else if (
+  process.env.NODE_ENV === "production" &&
+  typeof window !== "undefined"
+) {
+  module.exports = require("./prod-server");
 }
