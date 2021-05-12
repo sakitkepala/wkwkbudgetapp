bd3578bc3778e7bd9871acae972b303f7ea14f69
sakitkepala <andikapriyotama@gmail.com>
2021-05-12 14:37:50 +0700

    Hilangkan kondisi export dev server yang khusus development aja

--- src/test/server/index.js
+++ src/test/server/index.js
@@ -1,3 +1,7 @@
-if (process.env.NODE_ENV === "development" && typeof window !== "undefined") {
+// if (process.env.NODE_ENV === "development" && typeof window !== "undefined") {
+//   module.exports = require("./dev-server");
+// }
+
+if (typeof window !== "undefined") {
   module.exports = require("./dev-server");
 }
